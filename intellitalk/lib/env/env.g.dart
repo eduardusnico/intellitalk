d9c383698782967a8f5d0fbd1d3064431feef67e
eduardusnico <eduardusnico@gmail.com>
2023-06-12 15:17:15 +0700

    adding env rm gitignore

---
+++ intellitalk/lib/env/env.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+class _Env {
+  static const apiKey = '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';
+}
